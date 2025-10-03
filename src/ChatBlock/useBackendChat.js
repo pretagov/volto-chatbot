e9c6b73df70030933776e899f7bd4d127c965859
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2025-10-03 17:45:12 +0100

    bump timeouts

--- src/ChatBlock/useBackendChat.js
+++ src/ChatBlock/useBackendChat.js
@@ -299,8 +299,8 @@ class SubmitHandler {
 
     await delay(50);
 
-    const packetWarningTime = 15000;
-    const packetErrorTime = 25000;
+    const packetWarningTime = 25000;
+    const packetErrorTime = 60000;
     let warningTimeout = setTimeout(() => {
       this.handlePacketTimeout("Chat is taking a long time to reply.");
     }, packetWarningTime);
